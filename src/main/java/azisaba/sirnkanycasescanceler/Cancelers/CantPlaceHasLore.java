ab72d96fea1141331e06d32c532c67f384dea858
Sirnk <147326106+Sirnk@users.noreply.github.com>
2024-03-17 22:46:25 +0900

    initial commit

--- src/main/java/azisaba/sirnkanycasescanceler/Cancelers/CantPlaceHasLore.java
+++ src/main/java/azisaba/sirnkanycasescanceler/Cancelers/CantPlaceHasLore.java
@@ -1,6 +1,7 @@
 package azisaba.sirnkanycasescanceler.Cancelers;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,6 +9,7 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.List;
 import java.util.Objects;
 
 //Loreがついているアイテムの誤設置防止
@@ -19,10 +21,23 @@ public class CantPlaceHasLore implements Listener {
         if (itemInHand.hasItemMeta()) {
             ItemMeta meta = itemInHand.getItemMeta();
             String s = ChatColor.stripColor(Objects.requireNonNull(meta).getDisplayName());
+            Material m = event.getPlayer().getInventory().getItemInMainHand().getType();
             if (Objects.requireNonNull(meta).hasDisplayName() && s.contains("Storage Box")) {
                 return;
             }
             if (meta.hasDisplayName() && s.contains("Build-Tool")) {
+                return;
+            }
+            if (m == Material.DIAMOND_HOE || m == Material.IRON_HOE || m == Material.GOLDEN_HOE || m == Material.STONE_HOE || m == Material.WOODEN_HOE){
+                return;
+            }
+            if (m == Material.DIAMOND_SHOVEL || m == Material.IRON_SHOVEL || m == Material.GOLDEN_SHOVEL || m == Material.STONE_SHOVEL || m == Material.WOODEN_SHOVEL){
+                return;
+            }
+            if (m == Material.DIAMOND_AXE || m == Material.IRON_AXE || m == Material.GOLDEN_AXE || m == Material.STONE_AXE || m == Material.WOODEN_AXE){
+                return;
+            }
+            if (m == Material.DIAMOND_PICKAXE || m == Material.IRON_PICKAXE || m == Material.GOLDEN_PICKAXE || m == Material.STONE_PICKAXE || m == Material.WOODEN_PICKAXE){
                 return;
             }
             if (meta.hasLore()) {
--- src/main/java/azisaba/sirnkanycasescanceler/Cancelers/CantUseHasSection.java
+++ src/main/java/azisaba/sirnkanycasescanceler/Cancelers/CantUseHasSection.java
@@ -1,5 +1,6 @@
 package azisaba.sirnkanycasescanceler.Cancelers;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,15 +11,29 @@ import org.bukkit.inventory.meta.ItemMeta;
 //Loreがついているアイテムの誤設置防止
 public class CantUseHasSection implements Listener {
     @EventHandler
-    public void onInteractWithSectionedItem(PlayerInteractEntityEvent e) {
+    public void onInteractWithSectionedItemInMainHand(PlayerInteractEntityEvent e) {
         Player player = e.getPlayer();
         ItemStack item = player.getInventory().getItemInMainHand();
         ItemMeta meta = item.getItemMeta();
         if (meta == null) return;
-        String s = meta.getDisplayName();
+        String ms = meta.getDisplayName();
 
-        String type = item.getType().name();
-        if (s.contains("§") && type.equals("NAME_TAG")) {
+        Material type = item.getType();
+        if (ms.contains("§") && type == Material.NAME_TAG) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInteractWithSectionedItemInOffHand(PlayerInteractEntityEvent e) {
+        Player player = e.getPlayer();
+        ItemStack item = player.getInventory().getItemInOffHand();
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return;
+        String ms = meta.getDisplayName();
+
+        Material type = item.getType();
+        if (ms.contains("§") && type == Material.NAME_TAG) {
             e.setCancelled(true);
         }
     }
